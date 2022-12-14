a945c573d40fbe08d526a328b2563f0c232775be
Enver Cicak <enver.cicak@gmail.com>
2022-12-14 09:39:49 +0100

    Migrate unregister net device monitor to event-v2

--- caas/kubernetes/node/monitors-k8s-node.tf
+++ caas/kubernetes/node/monitors-k8s-node.tf
@@ -165,7 +165,7 @@ resource "datadog_monitor" "unregister_net_device" {
   count   = var.unregister_net_device_enabled == "true" ? 1 : 0
   name    = "${var.prefix_slug == "" ? "" : "[${var.prefix_slug}]"}[${var.environment}] Kubernetes Node Frequent unregister net device"
   message = coalesce(var.unregister_net_device_message, var.message)
-  type    = "event alert"
+  type    = "event-v2 alert"
 
   query = <<EOQ
     events('sources:kubernetes priority:all ${module.filter-tags.event_alert} \"UnregisterNetDevice\"').rollup('count').last('${var.unregister_net_device_timeframe}') > ${var.unregister_net_device_threshold_critical}
